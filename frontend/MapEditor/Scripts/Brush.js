88e67ca5043d3637d5eb5c02a451a7e389170c3d
Tyler Vandermeer <tyvdm9@gmail.com>
2021-12-05 16:59:52 -0600

    Restructure architecture

--- frontend/MapEditor/Scripts/Brush.js
+++
@@ -1,9 +0,0 @@
-class Brush {
-    constructor() {
-        this.tile;
-    }
-
-    setTile(tile) {
-        this.tile = tile;
-    }
-}
--- frontend/MapEditor/Scripts/constants.js
+++
@@ -1,2 +0,0 @@
-const tileTypeGrid = 'grid';
-const tileTypePalette = 'palette';
